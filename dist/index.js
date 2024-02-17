6908f3c28d9a5bf4f39b52245291a8c93a30c9f6
mjunaid249 <junaidch249249@gmail.com>
2024-02-17 20:36:37 +0500

    commit

--- dist/index.js
+++ dist/index.js
@@ -1,28 +1,38 @@
 "use strict";
-let funcObj = {
-    height: 3,
-    weight: 33,
-    Class: "Biochem",
-    func: (n, m) => {
-        console.log(n * m);
-    },
-};
-funcObj.func(30, 32);
-const MyObj = {
-    height: 32,
-    weight: 311,
-    gender: true,
-    func: (n, m) => {
-        console.log(n + m);
-    },
-    Class: "Biochemistry",
-};
-const obj = {
-    height: 33,
-    weight: 44,
-    gender: false,
-};
-const obj2 = {
-    height: 321,
-    weight: 21,
-};
+// interface Obj {
+//   height: number;
+//   weight: number;
+//   gender?: boolean; //Optional
+// }
+// type FuncType = (n: number, m: number) => void;
+// interface newObj extends Obj {
+//   Class: string;
+//   func: FuncType;
+// }
+// let funcObj: newObj = {
+//   height: 3,
+//   weight: 33,
+//   Class: "Biochem",
+//   func: (n, m) => {
+//     console.log(n * m);
+//   },
+// };
+// funcObj.func(30, 32);
+// const MyObj: newObj = {
+//   height: 32,
+//   weight: 311,
+//   gender: true,
+//   func: (n, m) => {
+//     console.log(n + m);
+//   },
+//   Class: "Biochemistry",
+// };
+// const obj: Obj = {
+//   height: 33,
+//   weight: 44,
+//   gender: false,
+// };
+// const obj2: Obj = {
+//   height: 321,
+//   weight: 21,
+// };
--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,43 @@
-interface Obj {
-  height: number;
-  weight: number;
-  gender?: boolean; //Optional
-}
+// interface Obj {
+//   height: number;
+//   weight: number;
+//   gender?: boolean; //Optional
+// }
 
-type FuncType = (n: number, m: number) => void;
-interface newObj extends Obj {
-  Class: string;
-  func: FuncType;
-}
+// type FuncType = (n: number, m: number) => void;
+// interface newObj extends Obj {
+//   Class: string;
+//   func: FuncType;
+// }
 
-let funcObj: newObj = {
-  height: 3,
-  weight: 33,
-  Class: "Biochem",
-  func: (n, m) => {
-    console.log(n * m);
-  },
-};
+// let funcObj: newObj = {
+//   height: 3,
+//   weight: 33,
+//   Class: "Biochem",
+//   func: (n, m) => {
+//     console.log(n * m);
+//   },
+// };
 
-funcObj.func(30, 32);
+// funcObj.func(30, 32);
 
-const MyObj: newObj = {
-  height: 32,
-  weight: 311,
-  gender: true,
-  func: (n, m) => {
-    console.log(n + m);
-  },
-  Class: "Biochemistry",
-};
+// const MyObj: newObj = {
+//   height: 32,
+//   weight: 311,
+//   gender: true,
+//   func: (n, m) => {
+//     console.log(n + m);
+//   },
+//   Class: "Biochemistry",
+// };
 
-const obj: Obj = {
-  height: 33,
-  weight: 44,
-  gender: false,
-};
+// const obj: Obj = {
+//   height: 33,
+//   weight: 44,
+//   gender: false,
+// };
 
-const obj2: Obj = {
-  height: 321,
-  weight: 21,
-};
+// const obj2: Obj = {
+//   height: 321,
+//   weight: 21,
+// };
