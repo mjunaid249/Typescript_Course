e6a28baff730847af1c8458d9aaf43ba21642b73
mjunaid249 <junaidch249249@gmail.com>
2024-02-17 20:32:48 +0500

    Objects

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,28 @@
 "use strict";
-const arr = [344, "Junaid"];
-const arr2 = ["Sami", "Zohaib", "Junaid"];
-const arr3 = [33, 42, 543, 32];
-console.log(arr, arr2, arr3);
-arr2.forEach((i) => {
-    console.log(i);
-});
-arr.forEach((e) => {
-    console.log(e.toString());
-});
-arr3.forEach((v) => {
-    console.log(v.toString());
-});
+let funcObj = {
+    height: 3,
+    weight: 33,
+    Class: "Biochem",
+    func: (n, m) => {
+        console.log(n * m);
+    },
+};
+funcObj.func(30, 32);
+const MyObj = {
+    height: 32,
+    weight: 311,
+    gender: true,
+    func: (n, m) => {
+        console.log(n + m);
+    },
+    Class: "Biochemistry",
+};
+const obj = {
+    height: 33,
+    weight: 44,
+    gender: false,
+};
+const obj2 = {
+    height: 321,
+    weight: 21,
+};
--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,30 @@ interface Obj {
   gender?: boolean; //Optional
 }
 
+type FuncType = (n: number, m: number) => void;
 interface newObj extends Obj {
   Class: string;
+  func: FuncType;
 }
+
+let funcObj: newObj = {
+  height: 3,
+  weight: 33,
+  Class: "Biochem",
+  func: (n, m) => {
+    console.log(n * m);
+  },
+};
+
+funcObj.func(30, 32);
 
 const MyObj: newObj = {
   height: 32,
   weight: 311,
   gender: true,
+  func: (n, m) => {
+    console.log(n + m);
+  },
   Class: "Biochemistry",
 };
 
